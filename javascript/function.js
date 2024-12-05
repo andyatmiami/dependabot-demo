1823dc5bb59c1cfadf185bf49b90dcc585f9ba16
Andy Stoneberg <andyatmiami@users.noreply.github.com>
2024-12-05 16:23:20 -0500

    add flaky test (#1)

--- javascript/function.js
+++ javascript/function.js
@@ -4,8 +4,12 @@ function someFunc(arg1, arg2) {
   return _.has(arg1, arg2);
 }
 
+function flaky(arg1, arg2) {
+  return Math.random() < 0.5
+}
+
 function dummyFunc() {
   return true
 }
 
-export { dummyFunc, someFunc }; 
+export { dummyFunc, someFunc, flaky }; 
