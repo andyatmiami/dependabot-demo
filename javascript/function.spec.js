acfcc01d59aea31efacf21dbae2f92d1c29255a4
Andy Stoneberg <astonebe@redhat.com>
2024-12-05 16:10:36 -0500

    add flaky test

--- javascript/function.spec.js
+++ javascript/function.spec.js
@@ -1,6 +1,11 @@
-import { dummyFunc } from "./function";
+import { dummyFunc, flaky } from "./function";
 
 test('baseline', () => {
         expect(dummyFunc()).toBe(true);
+    }
+);
+
+test('flaky', () => {
+        expect(flaky()).toBe(true);
     }
 );
